e53c252ca0f8718f4aae3bc6ee4f282d0de61934
Hussein Dawod <56936130+hdtv0199@users.noreply.github.com>
2022-07-11 01:36:33 -0400

    Create About.js

---
+++ components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const About = () => {
+    return(
+        <div className="about">
+            <h4>About</h4>
+            <p>I am in my early 20s trying to become a React Developer being self-taught. I hope
+            through many hours of learning and project making, I can achieve so.</p>
+        </div>
+    )
+}
+
+
+export default About
