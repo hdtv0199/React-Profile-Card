7de35be27297152be655078cc1a10da65fa97f17
Hussein Dawod <56936130+hdtv0199@users.noreply.github.com>
2022-07-11 01:35:55 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Info from './components/Info'
+import About from './components/About'
+import Interests from './components/Interests'
+import Footer from './components/Footer'
+
+const App = () => {
+    return(
+        <div className="container">
+            <Info />
+            <About />
+            <Interests />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
